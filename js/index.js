95ad2030a6f03c830c73ece8831f4ffab42cc7f0
Diego Bustos <diegobustos1229@gmail.com>
2019-11-25 18:00:35 -0800

    actualize sass

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
 
 
-$('.try').on('click', function(event){
+$('.sections').change(function(event){
     event.preventDefault();
-    // $('<div>sdfsadf</div>')
-    // .text('asdf');
     console.log('asdgf');
-})
+});
 
-// $('a').on('click', function(event) {
-//     event.preventDefault();
-//      $(this)
-//      .siblings('.head')
-//       .slideToggle('slow');
-//   });
-  
-
-console.log($('.hola'));
