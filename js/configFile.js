1c3db46d7768e2d7645bbbbc8b4bd56c53bb848a
Diego Bustos <diegobustos1229@gmail.com>
2019-11-25 20:06:10 -0800

    connect to API

---
+++ js/configFile.js
@@ -0,0 +1,32 @@
+
+export const KEY = '59GK5LDd0fEgFmvWNrEvuESYgA9V7ZwQ';
+export const NYT = 'https://api.nytimes.com/svc/topstories/v2/';
+export const SECTIONS = [ 
+    'Select your category',
+    'arts',
+    'automobiles',
+    'books',
+    'business',
+    'fashion',
+    'food',
+    'health',
+    'home',
+    'insider',
+    'magazine',
+    'movies',
+    'national',
+    'nyregion',
+    'obituaries',
+    'opinion',
+    'politics',
+    'realestate',
+    'science',
+    'sports',
+    'sundayreview',
+    'technology',
+    'theater',
+    'tmagazine',
+    'travel',
+    'upshot',
+    'world'
+]
--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 
+// // 
+import { KEY, NYT, SECTIONS } from './configFile.min.js';
 
+
+let sections = $('.sections');
+
+for (let i = 0; i < SECTIONS.length; i++) {
+    let category = SECTIONS[i]
+    let newSection = $(`<option value=${category}>${category}</option>`)
+    sections.append(newSection)
+}
+
+
+
+// Conect to 
 $('.sections').change(function(event){
-    event.preventDefault();
-    console.log('asdgf');
+    let option = $('.sections').val();
+    // let urt = NYT + option + '.json?api-key=' + KEY;
+    // console.log(urt);
+    $.ajax({
+        method: "GET",
+        url: NYT + option + '.json?api-key=' + KEY
+      }).done(function(data){
+        event.preventDefault();
+        const first = data.results[1];
+        console.log(first);
+      });
+
 });
 
